299c3bb62f93933fa3cd237478faccc85b152943
Sinscerly <email.thomas.jansema@gmail.com>
2017-12-05 08:52:03 +0100

    Update Infrared.h
    Deleted all the comments

--- Infrared.X/Infrared.h
+++ Infrared.X/Infrared.h
@@ -31,7 +31,7 @@ typedef union
     unsigned array[12];                 /* Used to loop through all struct members      */ 
 }IRbits;
 
-/****************************************************************************************************************************
+/********************************************************************************
         Notes about raw button codes that the uC receives 
 The code that the uC receives is 12 bits long.
 The first 3 bits(C1, C2 AND C3) stand firm programed. 
@@ -42,47 +42,12 @@ Then we get the next 3 bits.
 The we get the 6 last bits.
     Those bits are used for input data(D1 untill D6). There are 18 combinations,
     One key of H1, S1 or S2 and then one of the 6 last buttons.
-****************************************************************************************************************************/
-#define VOLUME_UP   (0b101100100000)    /* See Note VOLUME_UP                       */ 
-/****************************************************************************************************************************
-        Notes VOLUME_UP
-    Code: 101 100 100000
-    The volume+ button is connected to key number 1. 
-    So this is a Continue button so H needs to be up. Key number 1 is connected to the row of D1.
-****************************************************************************************************************************/
-#define VOLUME_DOWN (0b101100010000)    /* See Note VOLUME_Down                     */
-/****************************************************************************************************************************
-        Notes VOLUME_DOWN
-    Code: 101 100 010000
-    The volume- button is connected to key number 2.
-    So this is a Continue button so H needs to be up. Key number 2 is connected to the row of D2
-****************************************************************************************************************************/
-#define INPUT1      (0b101010010000)    /* See Note INPUT1                          */
-/****************************************************************************************************************************
-        Notes INPUT1
-    Code: 101 010 010000
-    The INPUT1 button is connected to key number 8.
-    So this is a Single-Shot button, connected on S1 so this one is up. Key number 8 is connected to the row of D2
-****************************************************************************************************************************/
-#define INPUT2      (0b101100001000)    /* See Note INPUT2                          */
-/****************************************************************************************************************************
-        Notes INPUT2
-    Code: 101 100 001000
-    The INPUT2 button is connected to key number 3.
-    So this is a Continue button so H needs to be up. Key number 3 is connected to the row of D3
-****************************************************************************************************************************/
-#define INPUT3      (0b101010100000)    /* See Note INPUT3                          */
-/****************************************************************************************************************************
-        Notes INPUT3
-    Code: 101 010 100000
-    The INPUT3 button is connected to key number 7.
-    So this is a Single-Shot button, connected on S1 so this one is up. Key number 7 is connected to the row of D1
-****************************************************************************************************************************/
-#define INPUT4      (0b101010001000)    /* See Note INPUT4                          */  
-/****************************************************************************************************************************
-        Notes INPUT4
-    Code: 101 010 001000
-    The INPUT1 button is connected to key number 9.
-    So this is a Single-Shot button, connected on S1 so this one is up. Key number 9 is connected to the row of D3
-****************************************************************************************************************************/
+********************************************************************************/
+#define VOLUME_UP   (0b101100100000)    /* key number 1, H  is up and D1 */ 
+#define VOLUME_DOWN (0b101100010000)    /* key number 2, H  is up and D2 */
+#define INPUT1      (0b101010010000)    /* key number 8, S1 is up and D2 */
+#define INPUT2      (0b101100001000)    /* key number 3, H  is up and D3 */
+#define INPUT3      (0b101010100000)    /* key number 7, S1 is up and D1 */
+#define INPUT4      (0b101010001000)    /* key number 9, S1 is up and D3 */  
+
 #endif
